5c96a7bae4656b2df5645bee911217c370bbba23
arbaazmeghani <arbaazmeghani@gmail.com>
2019-06-20 19:30:53 -0500

    Add 404 error for resources not found

--- src/index.js
+++ src/index.js
@@ -10,5 +10,9 @@ app.use((req, res, next) => {
 });
 app.use('/api/v1', contactsRoute);
 
+app.use((req, res, next) => {
+    res.status(404).send("404 Resource Not Found");
+});
+
 const port = process.env.port || 3000;
 app.listen(port, () => console.log("Running Express Server on Port: " + port));

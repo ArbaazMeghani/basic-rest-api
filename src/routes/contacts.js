a23432922a911fc1162dc293999a432a007bcdc3
arbaazmeghani <arbaazmeghani@gmail.com>
2019-06-20 19:15:52 -0500

    Add a simple route that handles a param property and a few simple query parameters for first/last name

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -2,7 +2,22 @@ let express = require('express')
 let router = express.Router();
 
 router.get('/contacts', (req,res) => {
-    res.send("You have requested all contacts")
+    if(req.query.firstname && req.query.lastname) {
+        res.send(`You have requested all contacts with the name: ${req.query.firstname} ${req.query.lastname}`)
+    }
+    else if(req.query.firstname) {
+        res.send(`You have requested all contacts with the first name: ${req.query.firstname}`)
+    }
+    else if(req.query.lastname) {
+        res.send(`You have requested all contacts with the last name: ${req.query.lastname}`)
+    }
+    else {
+        res.send("You have requested all contacts")
+    }
+});
+
+router.get('/contacts/:name', (req,res) => {
+    res.send(`You have requested a contact with the name: ${req.params.name}`)
 });
 
 module.exports = router

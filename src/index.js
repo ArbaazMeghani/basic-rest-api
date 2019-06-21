e7e8aa180300d13b2abaf837329123bc4e7b7a68
arbaazmeghani <arbaazmeghani@gmail.com>
2019-06-20 19:06:20 -0500

    setup prefix path so we can have are endpoint always start at /api/v1 and update our contacts routes.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ let express = require('express')
 let app = express();
 
 let contactsRoute = require('./routes/contacts')
-app.use(contactsRoute);
+app.use('/api/v1', contactsRoute);
 
 const port = process.env.port || 3000;
 app.listen(port, () => console.log("Running Express Server on Port: " + port));
--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,7 +1,7 @@
 let express = require('express')
 let router = express.Router();
 
-router.get('/api/v1/contacts', (req,res) => {
+router.get('/contacts', (req,res) => {
     res.send("You have requested all contacts")
 });
 

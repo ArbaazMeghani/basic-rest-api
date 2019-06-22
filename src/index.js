9421195ac9887e38315909d9e299b72cbf75d3e6
arbaazmeghani <arbaazmeghani@gmail.com>
2019-06-21 20:00:33 -0500

    update port

--- src/index.js
+++ src/index.js
@@ -23,5 +23,5 @@ app.use((req, res, next) => {
     res.status(404).send("404 Resource Not Found");
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Running Express Server on Port: " + port));
